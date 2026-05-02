eaba2980c61ae52323be72bc73e8bd3ab2a2f2a4
Sergio Rodríguez <srdz.af@gmail.com>
2026-05-02 00:48:22 -0600

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int l = 0, r = 0, ans = 0;
+        while(r < nums.size()-1){
+            int nmax = -1;
+            for(int i = l; i <= r; i++) nmax = max(nmax, i+nums[i]);
+            l = r+1;
+            r = nmax;
+            ans++;
+        }
+        return ans;
+    }
+};
